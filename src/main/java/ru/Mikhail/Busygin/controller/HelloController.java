a5ecc46aa2d228f2e2e71afe28006bb71efd434c
Busygin Mikhail <izerg556@gmail.com>
2023-01-22 11:58:07 +0400

    for auto CI/CD

--- src/main/java/ru/Mikhail/Busygin/controller/HelloController.java
+++ src/main/java/ru/Mikhail/Busygin/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String helloPage() {
         return "hello";
     }
